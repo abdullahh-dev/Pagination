fdedaa7cf1378670d5d544c3b20b5736b479e994
Abdullah <abdullahaha101@gmail.com>
2024-02-15 11:18:24 +0500

    added pagination logic

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Posts from './assets/components/Posts';
 function App() {
   const [products, setProducts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [productsPerPage, setProductsPerPage] = useState(10);
+  const [productsPerPage] = useState(5);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -16,9 +16,27 @@ function App() {
     };
     fetchProducts();
   }, []);
+
+  const indexofLastProduct = currentPage * productsPerPage;
+  const indexofFirstProduct = indexofLastProduct - productsPerPage;
+  const currentProducts = products.slice(
+    indexofFirstProduct,
+    indexofLastProduct
+  );
+
+  const paginate = (pagenumber) => {
+    setCurrentPage(pagenumber);
+  };
+
   return (
     <div>
-      <Posts products={products} />
+      <Posts products={currentProducts} />
+
+      <Pagination
+        productsPerPage={productsPerPage}
+        totalProducts={products.length}
+        paginate={paginate}
+      />
     </div>
   );
 }
--- src/assets/components/Pagination.jsx
+++ src/assets/components/Pagination.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
-function Pagination() {
-  return <div>Pagination</div>;
+function Pagination({ productsPerPage, totalProducts, paginate }) {
+  const pageNumber = [];
+  for (let i = 1; i <= Math.ceil(totalProducts / productsPerPage); i++) {
+    pageNumber.push(i);
+  }
+  return (
+    <nav className="flex justify-center mt-40">
+      <ul className="flex">
+        {pageNumber.map((number) => (
+          <li className="bg-gray-100 border  px-4 py-1 text-[24px]">
+            <a onClick={() => paginate(number)} key={number} href="#!">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
 export default Pagination;
